9b2a0b558d06f6b6b8489df5f969ec12722730c4
vladutpiele <v_piele04@yahoo.com>
2024-06-03 00:02:51 +0300

    cel mai tare commit

--- main.cpp
+++ main.cpp
@@ -27,30 +27,41 @@ int main(){
             Jucator* portar2 = new Portar("Portar2", 25, "portar", 36, 33, 29, 48, 45, 77, false, 91, 96);
             Jucator* fundas1 = new Fundas("Fundas1", 33, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
             Jucator* fundas2 = new Fundas("Fundas2", 22, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+
             fundas2->antreneaza();
             fundas2->antreneaza();
             fundas2->seAccidenteaza();
             fundas2->antreneaza();
+
             Jucator* fundas3 = new Fundas("Fundas3", 35, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
             Jucator* fundas4 = new Fundas("Fundas4", 36, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+
             Jucator* fundas5 = new Fundas("Fundas5", 29, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
             fundas5->afiseazaDetalii();
+
             Jucator* mijlocas1 = new Mijlocas("Mijlocas1", 31, "mijlocas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+
             Jucator* mijlocas2 = new Mijlocas("Mijlocas1", 34, "mijlocas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
             mijlocas2->afiseazaDetalii();
+
             Jucator* mijlocas3 = new Mijlocas("Mijlocas1", 29, "mijlocas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
             mijlocas3->afiseazaDetalii();
+
             Jucator* mijlocas4 = new Mijlocas("Mijlocas1", 22, "mijlocas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+
             Jucator* atacant1 = new Atacant("Atacant1", 22, "atacant", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), true);
             atacant1->antreneaza();
             atacant1->revineDupaAccidentare();
             atacant1->antreneaza();
             atacant1->antreneaza();
+
             Jucator* atacant2 = new Atacant("Atacant2", 29, "atacant", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), true);
             atacant2->revineDupaAccidentare();
             atacant2->antreneaza();
             atacant2->afiseazaDetalii();
+
             Jucator* atacant3 = new Atacant("Atacant3", 31, "atacant", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+
             std::vector<Jucator*> jucatori;
             jucatori.push_back(portar1);
             jucatori.push_back(portar2);
@@ -71,6 +82,20 @@ int main(){
             std::string numeEchipa = "Echipa";
             Echipa echipa(numeEchipa, 0, antrenor, jucatori);
             echipe.push_back(echipa);
+            delete portar1;
+            delete portar2;
+            delete fundas1;
+            delete fundas2;
+            delete fundas3;
+            delete fundas4;
+            delete fundas5;
+            delete mijlocas1;
+            delete mijlocas2;
+            delete mijlocas3;
+            delete mijlocas4;
+            delete atacant1;
+            delete atacant2;
+            delete atacant3;
         }
         Campionat ligaRomaniei(echipe);
         std::cout << "In campionat sunt " << Campionat::numarEchipe() << " echipe si " << Campionat::numarJucatori() << " jucatori" << '\n';
