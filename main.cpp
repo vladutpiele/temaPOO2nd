03f302893e55b0b48c3ac682e892602e5e21345f
vladutpiele <v_piele04@yahoo.com>
2024-06-03 01:26:27 +0300

    cel mai tare commit

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main(){
             /// voi crea 10 echipe in campionat
             Jucator* portar1 = new Portar("Portar1", 27, "portar", 30, 40, 22, 38, 55, 80, false, 97, 95);
             portar1->antreneaza();
-            portar1->seAccidenteaza();
+            //portar1->seAccidenteaza();
             portar1->antreneaza();
             Jucator* portar2 = new Portar("Portar2", 25, "portar", 36, 33, 29, 48, 45, 77, false, 91, 96);
             Jucator* fundas1 = new Fundas("Fundas1", 33, "fundas", 80, 88, 83, 77, 69, 66, false);
@@ -27,14 +27,16 @@ int main(){
 
             fundas2->antreneaza();
             fundas2->antreneaza();
-            fundas2->seAccidenteaza();
+            //fundas2->seAccidenteaza();
             fundas2->antreneaza();
+
 
             Jucator* fundas3 = new Fundas("Fundas3", 35, "fundas", 88, 66, 82, 77, 81, 80, false);
             Jucator* fundas4 = new Fundas("Fundas4", 36, "fundas", 60, 68, 75, 80, 59, 69, false);
 
             Jucator* fundas5 = new Fundas("Fundas5", 29, "fundas", 88, 77, 71, 79, 81, 72, false);
             fundas5->afiseazaDetalii();
+
 
             Jucator* mijlocas1 = new Mijlocas("Mijlocas1", 31, "mijlocas", 90, 88, 81, 86, 79, 77, false);
 
@@ -46,16 +48,17 @@ int main(){
 
             Jucator* mijlocas4 = new Mijlocas("Mijlocas1", 22, "mijlocas", 88, 82, 80, 90, 93, 91, false);
 
-            Jucator* atacant1 = new Atacant("Atacant1", 22, "atacant", 91, 90, 88, 86, 81, 83, true);
-            atacant1->antreneaza();
-            atacant1->revineDupaAccidentare();
+            Jucator* atacant1 = new Atacant("Atacant1", 22, "atacant", 91, 90, 88, 86, 81, 83, false);
+            //atacant1->antreneaza();
+            //atacant1->revineDupaAccidentare();
             atacant1->antreneaza();
             atacant1->antreneaza();
 
-            Jucator* atacant2 = new Atacant("Atacant2", 29, "atacant", 99, 96, 92, 88, 74, 88, true);
-            atacant2->revineDupaAccidentare();
+            Jucator* atacant2 = new Atacant("Atacant2", 29, "atacant", 99, 96, 92, 88, 74, 88, false);
+            //atacant2->revineDupaAccidentare();
             atacant2->antreneaza();
             atacant2->afiseazaDetalii();
+
 
             Jucator* atacant3 = new Atacant("Atacant3", 31, "atacant", 88, 99, 95, 95, 60, 75, false);
 
@@ -77,25 +80,14 @@ int main(){
             std::string numeAntrenor = "Antrenor";
             Antrenor antrenor(numeAntrenor, jucatori);
             std::string numeEchipa = "Echipa";
-            Echipa echipa(numeEchipa, 0, antrenor, jucatori);
+            std::vector<Jucator*> jucatori2;
             for(auto it : jucatori){
-                delete it;
+                jucatori2.push_back(it->clone());
             }
+            Echipa echipa(numeEchipa, 0, antrenor, jucatori2);
+
             teams.push_back(echipa);
-            delete portar1;
-            delete portar2;
-            delete fundas1;
-            delete fundas2;
-            delete fundas3;
-            delete fundas4;
-            delete fundas5;
-            delete mijlocas1;
-            delete mijlocas2;
-            delete mijlocas3;
-            delete mijlocas4;
-            delete atacant1;
-            delete atacant2;
-            delete atacant3;
+
         }
         Campionat ligaRomaniei(teams);
         std::cout << "In campionat sunt " << Campionat::numarEchipe() << " echipe si " << Campionat::numarJucatori() << " jucatori" << '\n';
