90f1b7a0c317b74ee10d15708c2988dac32e7c19
vladutpiele <v_piele04@yahoo.com>
2024-06-03 00:33:11 +0300

    cel mai tare commit

--- main.cpp
+++ main.cpp
@@ -11,10 +11,6 @@
 #include "Campionat.h"
 
 
-int getRandVal(){
-    return 70 + rand() % 30;
-}
-
 std::vector<Echipa> teams;
 
 int main(){
@@ -26,42 +22,42 @@ int main(){
             portar1->seAccidenteaza();
             portar1->antreneaza();
             Jucator* portar2 = new Portar("Portar2", 25, "portar", 36, 33, 29, 48, 45, 77, false, 91, 96);
-            Jucator* fundas1 = new Fundas("Fundas1", 33, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
-            Jucator* fundas2 = new Fundas("Fundas2", 22, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+            Jucator* fundas1 = new Fundas("Fundas1", 33, "fundas", 80, 88, 83, 77, 69, 66, false);
+            Jucator* fundas2 = new Fundas("Fundas2", 22, "fundas", 77, 75, 70, 81, 80, 92, false);
 
             fundas2->antreneaza();
             fundas2->antreneaza();
             fundas2->seAccidenteaza();
             fundas2->antreneaza();
 
-            Jucator* fundas3 = new Fundas("Fundas3", 35, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
-            Jucator* fundas4 = new Fundas("Fundas4", 36, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+            Jucator* fundas3 = new Fundas("Fundas3", 35, "fundas", 88, 66, 82, 77, 81, 80, false);
+            Jucator* fundas4 = new Fundas("Fundas4", 36, "fundas", 60, 68, 75, 80, 59, 69, false);
 
-            Jucator* fundas5 = new Fundas("Fundas5", 29, "fundas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+            Jucator* fundas5 = new Fundas("Fundas5", 29, "fundas", 88, 77, 71, 79, 81, 72, false);
             fundas5->afiseazaDetalii();
 
-            Jucator* mijlocas1 = new Mijlocas("Mijlocas1", 31, "mijlocas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+            Jucator* mijlocas1 = new Mijlocas("Mijlocas1", 31, "mijlocas", 90, 88, 81, 86, 79, 77, false);
 
-            Jucator* mijlocas2 = new Mijlocas("Mijlocas1", 34, "mijlocas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+            Jucator* mijlocas2 = new Mijlocas("Mijlocas1", 34, "mijlocas", 81, 84, 83, 85, 82, 90, false);
             mijlocas2->afiseazaDetalii();
 
-            Jucator* mijlocas3 = new Mijlocas("Mijlocas1", 29, "mijlocas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+            Jucator* mijlocas3 = new Mijlocas("Mijlocas1", 29, "mijlocas", 77, 76, 81, 87, 91, 93, false);
             mijlocas3->afiseazaDetalii();
 
-            Jucator* mijlocas4 = new Mijlocas("Mijlocas1", 22, "mijlocas", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+            Jucator* mijlocas4 = new Mijlocas("Mijlocas1", 22, "mijlocas", 88, 82, 80, 90, 93, 91, false);
 
-            Jucator* atacant1 = new Atacant("Atacant1", 22, "atacant", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), true);
+            Jucator* atacant1 = new Atacant("Atacant1", 22, "atacant", 91, 90, 88, 86, 81, 83, true);
             atacant1->antreneaza();
             atacant1->revineDupaAccidentare();
             atacant1->antreneaza();
             atacant1->antreneaza();
 
-            Jucator* atacant2 = new Atacant("Atacant2", 29, "atacant", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), true);
+            Jucator* atacant2 = new Atacant("Atacant2", 29, "atacant", 99, 96, 92, 88, 74, 88, true);
             atacant2->revineDupaAccidentare();
             atacant2->antreneaza();
             atacant2->afiseazaDetalii();
 
-            Jucator* atacant3 = new Atacant("Atacant3", 31, "atacant", getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), getRandVal(), false);
+            Jucator* atacant3 = new Atacant("Atacant3", 31, "atacant", 88, 99, 95, 95, 60, 75, false);
 
             std::vector<Jucator*> jucatori;
             jucatori.push_back(portar1);
